ef9c8e0307b1e118dc5e3c0887f317f6b92ce6f7
Journey to HaShem <journeytohashem@gmail.com>
2026-04-21 17:23:36 -0400

    refactor: extract LEARNING_PATH into data/lessons modules

---
+++ data/lessons/index.js
@@ -0,0 +1,7 @@
+import unit1 from './unit1.js';
+import unit2 from './unit2.js';
+import unit3 from './unit3.js';
+import unit4 from './unit4.js';
+import unit5 from './unit5.js';
+
+export const LEARNING_PATH = [unit1, unit2, unit3, unit4, unit5];
